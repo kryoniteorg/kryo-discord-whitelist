7796914140630462575c93dae71ed4aabe22501c
Kevin Huber <kevin.huber@hotmail.com>
2022-01-02 18:14:17 +0100

    Update KryoDiscordWhitelistBot

--- bot/src/main/java/org/kryonite/kryodiscordwhitelist/bot/KryoDiscordWhitelistBot.java
+++ bot/src/main/java/org/kryonite/kryodiscordwhitelist/bot/KryoDiscordWhitelistBot.java
@@ -13,20 +13,34 @@ import org.kryonite.kryodiscordwhitelist.common.persistence.repository.impl.Mari
 
 public class KryoDiscordWhitelistBot extends ListenerAdapter {
 
-  public static void main(String[] args) throws LoginException, InterruptedException, SQLException {
+  private final UserRepository userRepository;
+
+  public KryoDiscordWhitelistBot() throws SQLException {
     setupForkJoinPoolParallelism();
 
     Connection connection = DriverManager.getConnection(getEnv("CONNECTION_STRING"));
-    UserRepository userRepository = new MariaDbUserRepository(connection);
-
-    JDABuilder.createDefault(getEnv("TOKEN"))
-        .addEventListeners(new MessageListener(userRepository))
-        .setActivity(Activity.playing("Minecraft"))
-        .build()
-        .awaitReady();
+    userRepository = new MariaDbUserRepository(connection);
   }
 
-  private static String getEnv(String name) {
+  public KryoDiscordWhitelistBot(Connection connection) throws SQLException {
+    setupForkJoinPoolParallelism();
+    userRepository = new MariaDbUserRepository(connection);
+  }
+
+  public static void main(String[] args) throws LoginException, InterruptedException, SQLException {
+    KryoDiscordWhitelistBot kryoDiscordWhitelistBot = new KryoDiscordWhitelistBot();
+    kryoDiscordWhitelistBot.setupBot();
+  }
+
+  // There's currently a bug with Java 17: https://github.com/DV8FromTheWorld/JDA/issues/1858
+  private void setupForkJoinPoolParallelism() {
+    int cores = Runtime.getRuntime().availableProcessors();
+    if (cores <= 1) {
+      System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "1");
+    }
+  }
+
+  private String getEnv(String name) {
     String connectionString = System.getenv(name);
     if (connectionString == null) {
       connectionString = System.getProperty(name);
@@ -35,11 +49,11 @@ public class KryoDiscordWhitelistBot extends ListenerAdapter {
     return connectionString;
   }
 
-  // There's currently a bug with Java 17: https://github.com/DV8FromTheWorld/JDA/issues/1858
-  private static void setupForkJoinPoolParallelism() {
-    int cores = Runtime.getRuntime().availableProcessors();
-    if (cores <= 1) {
-      System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "1");
-    }
+  public void setupBot() throws LoginException, InterruptedException {
+    JDABuilder.createDefault(getEnv("TOKEN"))
+        .addEventListeners(new MessageListener(userRepository))
+        .setActivity(Activity.playing("Minecraft"))
+        .build()
+        .awaitReady();
   }
 }
