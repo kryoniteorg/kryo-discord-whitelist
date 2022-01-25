c91a2afb85696be9fef890d774fe17d05bebd486
Lukas Jost <lukas.jost@outlook.com>
2022-01-25 14:34:33 +0100

    feat: Increment version to 1.0.0 (#33)

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 
 allprojects {
     group = "org.kryonite"
-    version = "0.1.0"
+    version = "1.0.0"
 
     apply(plugin = "java-library")
     apply(plugin = "com.github.johnrengelman.shadow")
